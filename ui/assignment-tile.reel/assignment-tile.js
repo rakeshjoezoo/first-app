3bdfd181a46ef447018172184a4cc8f0390d72dd
rakeshjoezoo <noreply>
2014-11-03 22:40:09 +0000

    Add component assignmentTile

---
+++ ui/assignment-tile.reel/assignment-tile.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/assignment-tile.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class AssignmentTile
+ * @extends Component
+ */
+exports.AssignmentTile = Component.specialize(/** @lends AssignmentTile# */ {
+    constructor: {
+        value: function AssignmentTile() {
+            this.super();
+        }
+    }
+});
